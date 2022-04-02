1675dd02623a9ffdd965a5781f3a1a94365fd37b
Jeremi <jrynk@hotmail.com>
2022-04-02 15:57:25 +0200

    automating build process, some build errors still remian

--- build/install.sh
+++ build/install.sh
@@ -2,11 +2,3 @@
 
 cp -r /app/pebble-firmware/nrf/applications/Aries/ /app/pebble-firmware-legacy/nrf/applications/
 west build -b thingy91_nrf9160ns /app/pebble-firmware-legacy/nrf/applications/Aries/
-
-
-# RUN pip3 install -r zephyr/scripts/requirements.txt && \
-#     pip3 install -r nrf/scripts/requirements.txt && \
-#     pip3 install -r bootloader/mcuboot/scripts/requirements.txt
-
-# cd /app/pebble-firmware/ncs
-# west build -b thingy91_nrf9160ns /app/pebble-firmware-legacy/nrf/applications/asset_tracker/
