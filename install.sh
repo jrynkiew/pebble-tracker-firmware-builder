4f858f8b5974984b734ff23acdcea2c0af36bccb
Jeremi <jrynk@hotmail.com>
2022-04-08 11:47:15 +0200

    fixing repo

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+
 # # Environment variables
 red=`tput setaf 1`
 # purple=`tput setaf 93`
@@ -9,39 +9,41 @@ reset=`tput sgr0`
 
 . ./build/setenv.sh "applications/Aries/"
 
-git submodule update --init --recursive
+#git submodule update --init --recursive
 
-rm -rf ./external/pebble-firmware/ncs/nrf/boards/arm/thingy91_nrf9160 && \
-cp -rv /app/pebble-firmware-legacy/nrf/boards/arm/thingy91_nrf9160 /app/pebble-firmware/ncs/nrf/boards/arm/
+# rm -rf ./external/pebble-firmware/ncs/nrf/boards/arm/thingy91_nrf9160 && \
+# cp -rv ./external/pebble-firmware-legacy/nrf/boards/arm/thingy91_nrf9160 ./external/pebble-firmware/ncs/nrf/boards/arm/
 
-read -p "Which ${green}IoTeX${reset} ${gold}Pebble Tracker Firmware${reset} do you want to build?
-        (${red}1${reset}): Aries
+read -p "Which ${green}IoTeX${reset} ${gold}Pebble Tracker Firmware${reset} do you want to build? \n
+        (${red}1${reset}): Gravel
         (${red}2${reset}): Asset Tracker
+        (${red}3${reset}): Aries
         " FIRMWARE_SELECTION_CONFIG
 
 FIRMWARE_SELECTION=$(case "$FIRMWARE_SELECTION_CONFIG" in
-  (1)    
-    echo "aries"           ;;
-  (2)    
-    echo "asset_tracker"   ;;
+  (1)    echo "gravel"           ;;
+  (2)    echo "asset_tracker"    ;;
+  (3)    echo "aries"    ;;
 esac)
 
 case $FIRMWARE_SELECTION in
-  aries)
-    FIRMWARE_PATH="/app/pebble-firmware/nrf/applications/Aries/" ;;
+  gravel)
+    FIRMWARE_PATH="/app/pebble-firmware/nrf/applications/Gravel/" ;;
 
   asset_tracker)
     FIRMWARE_PATH="/app/pebble-firmware-legacy/nrf/applications/asset_tracker/" ;;
 
+  aries)
+    FIRMWARE_PATH="/app/pebble-firmware/nrf/applications/Aries/" ;;
   *)
     echo "${red}Not a valid answer. Terminating${reset}"
     exit 0 ;;
 esac
 
-echo "This script will attempt to install ${green}$BUILD $FIRMWARE_PATH ${reset}"
-
 # Getting necessary user input
-read -p "Do you want to perform a first-time installation (${red}F${reset}) or run the application (${green}R${reset})?: " MODE
+read -p "This script will attempt to install ${green}$BUILD $FIRMWARE_PATH ${reset}
+Do you want to perform a first-time installation (${red}F${reset}) or run the application (${green}R${reset})?: " MODE
+
 MODE_flag=$(case "$MODE" in
   (F|f)
     echo "docker-compose -p JRPC-pebble-firmware -f ./build/docker-compose.yaml up --no-deps --build" ;;
@@ -50,6 +52,3 @@ MODE_flag=$(case "$MODE" in
 esac)
 
 $MODE_flag
-
-
-
